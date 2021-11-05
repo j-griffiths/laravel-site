4b2f7bb61e1d79c0cfeb409a48f2de3a0bd4074d
jgriff6 <jofigriff@gmail.com>
2021-11-05 05:24:40 +0000

    Add Post model and seeder

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $post = new Post;
+        $post->title = "This is an example title.";
+        $post->content = "This is an example of content that a post could include.";
+        $post->user_id = 1;
+        $post->save();
+    }
+}
