06d552c22435ba49be59727a343e3d0724ffa20a
jgriff6 <jofigriff@gmail.com>
2021-11-05 09:27:17 +0000

    Add Profile model

---
+++ database/migrations/2021_11_05_091248_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('forename', 15);
+            $table->string('surname', 15);
+            $table->string('profession', 15);
+            $table->string('website', 30);
+            $table->string('biography', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
