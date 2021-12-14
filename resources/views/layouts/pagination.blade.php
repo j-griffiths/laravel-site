e94615c2fb80130c160493d2c0380f4fdbcdf795
jgriff6 <jofigriff@gmail.com>
2021-12-14 21:13:46 +0000

    Cleanup pagination layout

--- resources/views/layouts/pagination.blade.php
+++ resources/views/layouts/pagination.blade.php
@@ -1,14 +1,10 @@
 <div id="list">
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" v-for="item in paginatedData">
-                <a :href="setLink(item)">
-                    <div class="p-6 bg-white border-b border-gray-200 hover:bg-gray-50">
-                        @{{ item . title }}
-                    </div>
-                </a>
+    <div v-for="item in paginatedData">
+        <a :href="setLink(item)">
+            <div class="p-4 border-b hover:bg-gray-50">
+                @{{ item.title }}
             </div>
-        </div>
+        </a>
     </div>
 
     <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
