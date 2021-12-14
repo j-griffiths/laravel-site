55c2cc9f444c39c36a9026fb449850bdc48c51db
jgriff6 <jofigriff@gmail.com>
2021-12-14 23:43:14 +0000

    Implement errors and image upload for posts.create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,18 +8,18 @@
         Create New Post
     </x-slot>
 
-    {{-- <form method="POST" action="{{ route('posts.store') }}">
-        @csrf
-        <div class="flex flex-col">
-        <p> Title: <input type="text" name="title" value="{{ old('title') }}"> </p>
-        <p> Content: <input type="text" name="content" value="{{ old('content') }}"> </p>
-        <p> Upload Image (Optional): <input type="file" name="uploadedFile" value="{{ old('uploadedFile') }}"> </p>
-        <p> <input type="submit" value="Create"> </p> 
-        <p> <a href="{{ route('posts.index') }}"> Cancel </a> </p>
+    @if ($errors->any())
+        <div>
+            Errors:
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li> {{ $error }} </li>
+                @endforeach
+            </ul>
         </div>
-    </form> --}}
+    @endif
 
-    <form method="POST" action="{{ route('posts.store') }}">
+    <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
         @csrf
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -37,9 +37,7 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-image">
                     Upload Image
                 </label>
-                <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight"
-                    id="grid-image" type="file" name="uploadedFile" value="{{ old('uploadedFile') }}">
+                <input  type="file" name="uploadedFile" accept="image/*">
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -50,8 +48,7 @@
                 <textarea
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 h-80 leading-tight 
                     focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-content" placeholder="Example Content" name="content"
-                    value="{{ old('content') }}"></textarea>
+                    id="grid-content" type="text" placeholder="Example Content" name="content">{{ old('content') }}</textarea>
             </div>
         </div>
         <div class="flex items-center">
