d10857d531673b758dd2fc9e4fef2f08c0ce7aea
jgriff6 <jofigriff@gmail.com>
2021-12-14 21:12:40 +0000

    Update default app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,13 +25,23 @@
         <!-- Page Heading -->
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ $header }}
+                </h2>
             </div>
         </header>
 
         <!-- Page Content -->
         <main>
-            {{ $slot }}
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 bg-white border-b border-gray-200">
+                            {{ $slot }}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
