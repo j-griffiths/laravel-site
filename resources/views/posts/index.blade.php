0bce96aeaafd2e68163fd1dc13da356613e24079
jgriff6 <jofigriff@gmail.com>
2021-12-14 21:13:12 +0000

    Cleanup posts.index view layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,16 +5,16 @@
     </x-slot>
 
     <x-slot name="header">
-        <h2 class="flex items-center clearfix">
-            <div class="font-semibold text-xl text-gray-800 leading-tight flex-1">
-            {{ __('Posts') }}
+        <div class="flex items-center">
+            <div class="flex-1">
+                {{ __('Posts') }}
             </div>
             <a href="posts/create" class="float-right bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                 Create Post
             </a> 
-        </h2>
+        </div>
     </x-slot>
 
-    @include('layouts.paginater', ['items' => $posts])
+    @include('layouts.pagination', ['items' => $posts])
 
 </x-app-layout>
