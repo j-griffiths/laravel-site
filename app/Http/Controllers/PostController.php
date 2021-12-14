a675f27fb42eba74fb45c460f92df2ff31fa73e9
jgriff6 <jofigriff@gmail.com>
2021-12-14 23:51:03 +0000

    Implement store method for PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,7 +35,36 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $profid = auth()->user()->id;
+        $request->merge(['profile_user_id' => $profid]);
+        $request->validate([
+            'uploadedFile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:4096',
+        ]);
+
+        $path = null;
+        if ($request->hasFile('uploadedFile')) {
+            if ($request->file('uploadedFile')->isValid()) {
+                $path = $request->uploadedFile->store('public/images');
+            }
+        }
+        $request->merge(['imagePath' => $path]);
+
+        $validatedData = $request->validate([
+            'title' => 'required|max:300',
+            'content' => 'required|max:10000',
+            'profile_user_id' => 'required|integer',
+            'imagePath' => 'nullable',
+        ]);
+
+        $post = new Post;
+        $post->title = $validatedData['title'];
+        $post->content = $validatedData['content'];
+        $post->profile_user_id = $validatedData['profile_user_id'];
+        $post->imagePath = $validatedData['imagePath'];
+        $post->save();
+
+        session()->flash('message', 'Post was created successfully.');
+        return redirect()->route('posts.show', ['post' => $post]);
     }
 
     /**
