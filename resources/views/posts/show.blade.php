b2370b87ea103a7e7ee2fc9600a3f3c6647b2e53
jgriff6 <jofigriff@gmail.com>
2021-12-15 02:50:58 +0000

    Implement displaying images in posts.show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,15 @@
         {{ $post->title }}
     </x-slot>
 
+    {{$post->profile_user_id}}
+
     {{$post->content}}
+
+    @if ($post->imagePath)
+        <img src="../{{ $post->imagePath }}" />
+    @endif
+    
+    {{$post->created_at}}
 
 </x-app-layout>
 
