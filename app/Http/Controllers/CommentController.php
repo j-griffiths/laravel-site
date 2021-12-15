d1df74d180a3ec7eb5428b839dc945a98d129f10
jgriff6 <jofigriff@gmail.com>
2021-12-15 21:24:11 +0000

    Add API CommentController, implement store method

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Post;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'comment'   => 'required|max:5000',
+                'post_id' => 'required|integer|exists:posts,id'
+            ]);
+        }
+        catch (ValidationException $exception) {
+            return response()->json([
+                'status' => 'error',
+                'msg'    => 'Error',
+                'errors' => $exception->errors(),
+            ], 422);
+        }
+
+        $comment = new Comment();
+        $comment->content = $request->comment;
+        $post = Post::find($request->post_id);
+        $comment->profile()->associate(auth()->user()->profile);
+        $post->comments()->save($comment);
+        
+        return response()->json([
+            'status' => 'success',
+            'msg'    => 'Okay',
+            'comment' => $comment,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
