d02b8e365f908659c623589a2c5702313609b106
jgriff6 <jofigriff@gmail.com>
2021-12-16 20:15:36 +0000

    Route like requests correctly

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LikeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('comments', CommentController::class, ['as' => 'api'])->middleware(['auth:sanctum']);
 
 Route::get('/profiles/{id}', [ProfileController::class, 'apiShow'])->name('api.profiles.show')->middleware(['auth:sanctum']);
+
+Route::post('/likes', [LikeController::class, 'store'])->name('api.likes.store')->middleware(['auth:sanctum']);
+
+// This delete route uses post so that it can pass a request for validation. As likes are represented by a pivot table without an id of their own,
+// a request to delete them must come with parameters which must be validated, before trying to find and delete an entry.
+Route::post('/likes/delete', [LikeController::class, 'destroy'])->name('api.likes.destroy')->middleware(['auth:sanctum']);
